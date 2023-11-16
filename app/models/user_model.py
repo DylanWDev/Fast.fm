1d7cb1adbd972324160290aa3006e87b098e3be8
Dylan Wyrick <xpoweroutage@gmail.com>
2023-11-16 14:56:11 +0000

    started on code

---
+++ app/models/user_model.py
@@ -0,0 +1,2 @@
+from sqlalchemy import Column, String, Integer, Date, ForeignKey
+from sqlalchemy.orm import relationship
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
-
+import app.models
+import app.api
+from database import engine
 app = FastAPI()
 
+models.Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 async def root():
